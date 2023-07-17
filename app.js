1d1824fdb9edf4072abc48fb90c2902a297eb5e3
igizeneza <139802729+igizeneza@users.noreply.github.com>
2023-07-17 22:28:18 +0200

    commit
    yaWEEEE

---
+++ app.js
@@ -0,0 +1,12 @@
+// 80% of storage used … You can clean up space or get more storage for Drive, Gmail, and Google Photos.
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener('click', () => {
+    container.classList.add("sign-up-mode");
+});
+sign_in_btn.addEventListener('click', () => {
+    container.classList.remove("sign-up-mode");
+});
